1ca0138332277805a34b9f2dad2f0d891335668d
Adam Swieciak <adamswiec@gmail.com>
2020-02-09 10:15:44 +0100

    first css project animation

---
+++ programm/codevening-start/js/popup.js
@@ -0,0 +1,10 @@
+// console.log("akademia 108");
+const showPopup= (event) => {
+    event.preventDefault();
+    document.getElementById("popup").style.display="block";
+}
+const hidePopup= (event) => {
+    event.preventDefault();
+    document.getElementById("popup").style.display="none";
+}
+
